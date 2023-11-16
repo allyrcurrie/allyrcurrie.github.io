5dd083ac5e847af6328196cf32c861a4a015a703
allyrcurrie <142851980+allyrcurrie@users.noreply.github.com>
2023-11-15 18:58:17 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const redpanda = document.getElementsByClassName("redpanda");
+const hamburgerButton = document.querySelector('[data-ja="hamburger"]');
+const nav = document.querySelector('nav');
+hamburgerButton.addEventListener('click', function(){
+    nav.classList.toggle("active");
+
+})
