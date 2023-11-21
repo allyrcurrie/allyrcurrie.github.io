b08b0ecfeac51d3f4d0a79c6f9f3d982436b01f2
allyrcurrie <142851980+allyrcurrie@users.noreply.github.com>
2023-11-20 20:12:38 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const hamburgerButton = document.querySelector('[data-ja="hamburger"]');
+
 const nav = document.querySelector('nav');
+
 hamburgerButton.addEventListener('click', function(){
     nav.classList.toggle("active");
+    hamburgerButton.classList.toggle("active")
 
 })
